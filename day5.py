32d375298687a778094a205a35f6dd253e7a312b
Laith Malky <laith.malky@gmail.com>
2024-12-27 13:35:51 +0200

    commit

--- day5.py
+++ day5.py
@@ -31,3 +31,16 @@ for score in student_score:
 print(max_score)
 
 print(max(student_score))
+
+
+# the range is a way for a loop without a list
+for number in range(1,11,3): # it will start in 1, it will end in -1 means in 10, 3 is for the step long in here 3 steps 
+    print(number)
+
+
+# a task for find the sum of numbers from 1 to 100 
+sum = 0
+for number in range(1,101):
+    sum += number
+
+print(sum)
